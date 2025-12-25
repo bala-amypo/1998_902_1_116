b313055bbc2cb3b0fe62029c5295326139594121
bala-amypo <bala@amypo.in>
2025-12-25 14:00:04 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/BreachReportDto.java
@@ -0,0 +1,43 @@
+package com.example.demo.dto;
+
+import java.math.BigDecimal;
+
+public class BreachReportDto {
+
+    private Long contractId;
+    private Integer daysDelayed;
+    private BigDecimal penaltyAmount;
+    private String reportStatus;
+
+    public Long getContractId() {
+        return contractId;
+    }
+
+    public void setContractId(Long contractId) {
+        this.contractId = contractId;
+    }
+
+    public Integer getDaysDelayed() {
+        return daysDelayed;
+    }
+
+    public void setDaysDelayed(Integer daysDelayed) {
+        this.daysDelayed = daysDelayed;
+    }
+
+    public BigDecimal getPenaltyAmount() {
+        return penaltyAmount;
+    }
+
+    public void setPenaltyAmount(BigDecimal penaltyAmount) {
+        this.penaltyAmount = penaltyAmount;
+    }
+
+    public String getReportStatus() {
+        return reportStatus;
+    }
+
+    public void setReportStatus(String reportStatus) {
+        this.reportStatus = reportStatus;
+    }
+}
