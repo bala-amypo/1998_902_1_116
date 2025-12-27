8311f5125157bbcc4f5c988116d4ea51a559ce03
bala-amypo <bala@amypo.in>
2025-12-27 09:22:47 +0000

    User commit

--- src/main/java/com/example/demo/entity/Contract.java
+++ src/main/java/com/example/demo/entity/Contract.java
@@ -1,7 +1,6 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.*;
-import java.time.LocalDate;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "contracts")
@@ -13,49 +12,22 @@ public class Contract {
 
     private String contractNumber;
 
-    private LocalDate startDate;
-
-    private LocalDate endDate;
-
     public Contract() {
-    }
-
-    public Contract(Long id, String contractNumber, LocalDate startDate, LocalDate endDate) {
-        this.id = id;
-        this.contractNumber = contractNumber;
-        this.startDate = startDate;
-        this.endDate = endDate;
     }
 
     public Long getId() {
         return id;
     }
- 
+
     public void setId(Long id) {
         this.id = id;
     }
- 
+
     public String getContractNumber() {
         return contractNumber;
     }
- 
+
     public void setContractNumber(String contractNumber) {
         this.contractNumber = contractNumber;
-    }
- 
-    public LocalDate getStartDate() {
-        return startDate;
-    }
- 
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
-    }
- 
-    public LocalDate getEndDate() {
-        return endDate;
-    }
- 
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
     }
 }
